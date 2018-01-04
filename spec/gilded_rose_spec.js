20752c0105b3e4603f0e958b70b7617e14c107cf
majdeddine <majda-jambouri@hotmail.fr>
2018-01-04 14:57:29 +0000

    add tests for backstage passes

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -44,4 +44,30 @@ describe('Gilded Rose', () => {
       expect(items[0].sellIn).toEqual(0);
     });
   });
+
+  describe('Backstage passes', () => {
+    it('qualtiy should increase by one', () => {
+      const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 11, 10)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toEqual(11);
+    });
+
+    it('qualtiy should increase by two', () => {
+      const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 10, 11)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toEqual(13);
+    });
+
+    it('qualtiy should increase by three', () => {
+      const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 5, 11)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toEqual(14);
+    });
+
+    it('qualtiy should return 0', () => {
+      const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 0, 11)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toEqual(0);
+    });
+  });
 });
