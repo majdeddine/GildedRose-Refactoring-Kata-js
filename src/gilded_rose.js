84225209cd77869618fbea97877f1e7ef1bb4e77
majdeddine <majda-jambouri@hotmail.fr>
2018-01-30 19:42:30 +0000

    add function to update expired items

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -41,6 +41,26 @@ class Shop {
     }
   }
 
+  updateExpiredItems(i){
+    if (this.items[i].sellIn < 0) {
+      if (this.items[i].name != 'Aged Brie') {
+        if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+          if (this.items[i].quality > 0) {
+            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+              this.items[i].quality = this.items[i].quality - 1;
+            }
+          }
+        } else {
+          this.items[i].quality = this.items[i].quality - this.items[i].quality;
+        }
+      } else {
+        if (this.items[i].quality < 50) {
+          this.items[i].quality = this.items[i].quality + 1;
+        }
+      }
+    }
+  }
+
 
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
@@ -57,23 +77,7 @@ class Shop {
 
       this.updateSellIn(i);
 
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
-      }
+      this.updateExpiredItems(i);
 
 
     }
