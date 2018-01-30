64141deed356ab32ef34e910028f42d9561dfcdb
majdeddine <majda-jambouri@hotmail.fr>
2018-01-30 19:29:35 +0000

    extract function to update aged brie and backstage passes

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -13,9 +13,28 @@ class Shop {
 
   updateNormalItem(i) {
     if (this.items[i].quality > 0) {
-    this.items[i].quality = this.items[i].quality - 1;
+    this.items[i].quality -= 1;
     }
   }
+
+  updateOtherItems(i) {
+    if (this.items[i].quality < 50) {
+      this.items[i].quality = this.items[i].quality + 1;
+      if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+        if (this.items[i].sellIn < 11) {
+          if (this.items[i].quality < 50) {
+            this.items[i].quality = this.items[i].quality + 1;
+          }
+        }
+        if (this.items[i].sellIn < 6) {
+          if (this.items[i].quality < 50) {
+            this.items[i].quality = this.items[i].quality + 1;
+          }
+        }
+      }
+    }
+  }
+
 
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
@@ -26,21 +45,7 @@ class Shop {
             this.updateNormalItem(i)
           }
       } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
+          this.updateOtherItems(i);
       }
 
 
