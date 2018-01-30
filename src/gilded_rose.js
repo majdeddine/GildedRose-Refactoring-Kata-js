3d7e60a3106a8bb1984f28dfb37d0645684e428f
majdeddine <majda-jambouri@hotmail.fr>
2018-01-30 19:38:03 +0000

    extract a function to update sellIn date

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -35,6 +35,12 @@ class Shop {
     }
   }
 
+  updateSellIn(i){
+    if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+      this.items[i].sellIn = this.items[i].sellIn - 1;
+    }
+  }
+
 
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
@@ -49,9 +55,8 @@ class Shop {
       }
 
 
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
+      this.updateSellIn(i);
+
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
